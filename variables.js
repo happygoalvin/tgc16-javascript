d57cad998b97c6c71e45474997666070a63b0695
happygoalvin <yo.alvin.96@gmail.com>
2022-01-26 03:26:08 +0000

    updated variables.js

--- variables.js
+++ variables.js
@@ -1,1 +1,35 @@
 console.log ("hello world")
+
+//data types in javascript
+//numbers
+//floats
+
+// integers
+//42
+
+// strings
+// 'she sells seashells by the seashore'
+// 'jack and jill went up the hill'
+// 'asd1234'
+
+
+// boolean values
+//true or false
+
+// variables
+// variables basically stored values
+
+// create a js variable
+let secretoflife = 42;
+let super_long_variable_name = 15;
+console.log(secretoflife);
+
+// imagine console to be a container that can have many functions
+// and log is one of those functions the dot means 'belong to'.
+// think of the . as the 's
+// console.log ==> console's log function
+
+// you can 'let' a variable once (*normally)
+// let secretoflife = 43;
+secretoflife = 43;
+console.log ("secretoflife =", secretoflife);
