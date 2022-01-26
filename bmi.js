2ccff66b84982e7b6f160765211e1480ca5cbac8
happygoalvin <yo.alvin.96@gmail.com>
2022-01-26 06:23:38 +0000

    Deleted duplicate files, created scope.js

--- bmi.js
+++
@@ -1,8 +0,0 @@
-const prompt = require('prompt-sync')();
-
-// eqv in Python:
-// float(input("Please enter your weight"))
-let weight = parseFloat(prompt("Please enter your weight"));
-let height = parseFloat(prompt("Please enter your height"));
-let bmi = weight / height ** 2;
-console.log("bmi =", bmi)
