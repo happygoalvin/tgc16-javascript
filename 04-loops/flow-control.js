27720cf62956eebadf5d317ed85f77763c40a4de
happygoalvin <yo.alvin.96@gmail.com>
2022-01-27 18:16:56 +0800

    added flow-control and for-loop

---
+++ 04-loops/flow-control.js
@@ -0,0 +1,22 @@
+// let n = 0;
+// while (n<10) {
+//     console.log(n);
+//     if (n>5) {
+//         break; // exit the loop
+//         // and go to the next line
+//         // after the loop
+//     }
+//     n++;
+// }
+// console.log("loop ended")
+
+let i = 0;
+let j = 0;
+while (i < 3) {
+         if (j == 1) {
+           break;
+          }
+         j++;
+      i++;  
+    }
+    
