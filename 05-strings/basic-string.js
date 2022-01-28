663f4250d3e8c8adf32fcd9783e63b37f2bab5b1
happygoalvin <yo.alvin.96@gmail.com>
2022-01-28 10:37:01 +0800

    added basic-strings.js

---
+++ 05-strings/basic-string.js
@@ -0,0 +1,11 @@
+// strings begin with a single'' or double"" quote
+let words = "The quick brown fox jumps over the lazy dog";
+let rhyme = "She sells seashells by the seashore";
+let paragraph = "don't kill me I don't know anything";
+let story = 'She said, "I sell seashells by the seashore"'
+let story2 = 'She said,"I don\'t know anything"';
+let story3 = "He said, \"gong xi fa cai\"";
+console.log("A\nB")
+console.log("Social\tDistancing")
+console.log("I wish you a merry christmas\nAnd a happy new year\nfor 2022\n")
+console.log("C:\\documents\\tests");
